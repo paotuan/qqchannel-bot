fd5a7267018800ed63edbbc54b78b5fb252f4789
maliut <lqn619@163.com>
2022-09-26 01:12:34 +0800

    feat: production 模式下启动静态服务器

--- server/index.ts
+++ server/index.ts
@@ -5,3 +5,11 @@ import './service/log'
 import './service/note'
 import './service/card'
 import './service/dice'
+import * as express from 'express'
+
+if (process.env.NODE_ENV === 'production') {
+  const server = express()
+  server.use(express.static('dist'))
+  server.listen(4175)
+  console.log('后台已启动，请访问 http://localhost:4175')
+}
