5ca0dd0eb5d507cb6990a6f11e22138f2f660bab
maliut <lqn619@163.com>
2023-01-04 18:12:16 +0800

    feat: 地图编辑工具栏

--- src/views/ScenePanel/MapStage.vue
+++ src/views/ScenePanel/MapStage.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="flex-grow py-4">
-    <input type="file" name="filename" accept="image/gif,image/jpeg,image/jpg,image/png,image/svg" @change="handleFile" />
+<!--    <input type="file" name="filename" accept="image/gif,image/jpeg,image/jpg,image/png,image/svg" @change="handleFile" />-->
     <div ref="container" class="w-full h-full"></div>
     <!-- toolbar -->
-    <div>
+    <div class="fixed bottom-0 mx-auto">
       <div>
-
+        <template v-if="toolbarItem === 'map'">
+          <MapTool :layer="layer" />
+        </template>
       </div>
       <div class="flex gap-4">
         <button class="btn btn-square" :class="{ 'btn-outline': toolbarItem !== 'map' }" @click="selectToolbar('map')">
@@ -17,6 +19,7 @@
         <button class="btn btn-square" :class="{ 'btn-outline': toolbarItem !== 'text' }" @click="selectToolbar('text')">
           <PencilIcon class="h-6 w-6" />
         </button>
+        <!-- todo save btn -->
       </div>
     </div>
   </div>
@@ -25,6 +28,7 @@
 import { onMounted, ref, shallowRef } from 'vue'
 import Konva from 'konva'
 import { MapIcon, MapPinIcon, PencilIcon } from '@heroicons/vue/24/outline'
+import MapTool from './toolbar/MapTool.vue'
 
 // container elem
 const container = ref<HTMLDivElement>()
@@ -36,7 +40,6 @@ const selectToolbar = (item: ToolbarItem) => toolbarItem.value = item === toolba
 
 // init stage
 const layer = shallowRef(new Konva.Layer())
-const background = shallowRef() // Konva.Image
 onMounted(() => {
   const stage = new Konva.Stage({
     container: container.value!,
@@ -45,39 +48,6 @@ onMounted(() => {
     height: 500 // todo
   })
   stage.add(layer.value)
-  // const circle = new Konva.Circle({
-  //   x: stage.width() / 2,
-  //   y: stage.height() / 2,
-  //   radius: 70,
-  //   fill: 'red',
-  //   stroke: 'black',
-  //   strokeWidth: 4
-  // })
-  // layer.value.add(circle)
 })
-
-const handleFile = (e: Event) => {
-  const files = (e.target as HTMLInputElement).files
-  const reader = new FileReader()
-  reader.onload = (e) => {
-    const imageUrl = e.target!.result
-    Konva.Image.fromURL(imageUrl, (node: Konva.Image) => {
-      const attrs = {
-        x: 0,
-        y: 0,
-        scaleX: 0.5,
-        scaleY: 0.5,
-        // draggable: true
-      }
-      console.log(attrs)
-      node.setAttrs(attrs)
-      // node.setAttr('data-src', imageUrl)
-      layer.value.removeChildren()
-      layer.value.add(node)
-      background.value = node
-    })
-  }
-  reader.readAsDataURL(files![0])
-}
 </script>
 
---
+++ src/views/ScenePanel/toolbar/MapTool.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="flex gap-2">
+    <input ref="realUploadBtn" type="file" name="filename" accept="image/gif,image/jpeg,image/jpg,image/png,image/svg" class="hidden" @change="handleFile" />
+    <template v-if="!background">
+      <button class="btn gap-2" @click="uploadBackground">
+        <PhotoIcon class="w-6 h-6" />上传图片
+      </button>
+    </template>
+    <template v-else>
+      <button class="btn btn-square" @click="uploadBackground">
+        <PhotoIcon class="w-6 h-6" />
+      </button>
+      <input :value="scale" type="range" min="0.1" max="2" step="0.01" class="range range-xs" @input="onScaleChange" />
+      <button class="btn btn-square" @click="clearBackground">
+        <XMarkIcon class="w-6 h-6" />
+      </button>
+    </template>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref, shallowRef } from 'vue'
+import Konva from 'konva'
+import { PhotoIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+
+interface Props {
+  layer: Konva.Layer
+}
+
+const props = defineProps<Props>()
+
+const realUploadBtn = ref<HTMLInputElement>()
+const background = shallowRef<Konva.Image | null>(null) // Konva.Image
+const scale = ref(0.5)
+
+const handleFile = (e: Event) => {
+  const files = (e.target as HTMLInputElement).files
+  const reader = new FileReader()
+  reader.onload = (e) => {
+    const imageUrl = e.target!.result
+    Konva.Image.fromURL(imageUrl, (node: Konva.Image) => {
+      const attrs = {
+        x: 0,
+        y: 0,
+        scaleX: scale.value,
+        scaleY: scale.value,
+        // draggable: true
+      }
+      node.setAttrs(attrs)
+      // node.setAttr('data-src', imageUrl)
+      props.layer.removeChildren()
+      props.layer.add(node)
+      background.value = node
+    })
+  }
+  reader.readAsDataURL(files![0])
+}
+
+const onScaleChange = (e: Event) => {
+  scale.value = Number((e.target as HTMLInputElement).value)
+  background.value?.scale({ x: scale.value, y: scale.value })
+}
+
+const uploadBackground = () => {
+  realUploadBtn.value?.click()
+}
+
+const clearBackground = () => {
+  props.layer.removeChildren()
+  background.value = null
+  scale.value = 0.5
+}
+</script>
+
