f06881592b3f1c75326cefd36ef44ea2f8042968
maliut <lqn619@163.com>
2023-01-27 00:33:43 +0800

    feat: 上传 npc 头像

--- src/views/ScenePanel/battle/CharacterListItemNpc.vue
+++ src/views/ScenePanel/battle/CharacterListItemNpc.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="flex gap-2 h-12">
-    <div class="avatar placeholder">
-      <div class="w-12 rounded-full bg-neutral-focus text-neutral-content">
-<!--        <img :src="userInfo.avatar" :alt="userInfo.nick" />-->
-        <span>{{ props.chara.name.slice(0, 2) }}</span>
-      </div>
+    <div class="avatar" :class="{ 'placeholder': !props.chara.avatar }" @click.stop="uploadAvatar">
+      <template v-if="props.chara.avatar">
+        <div class="w-12 rounded-full">
+          <img :src="props.chara.avatar" :alt="props.chara.name" />
+        </div>
+      </template>
+      <template v-else>
+        <div class="w-12 rounded-full bg-neutral-focus text-neutral-content">
+          <span>{{ props.chara.name.slice(0, 2) }}</span>
+        </div>
+      </template>
+      <input ref="realUploadBtn" type="file" name="filename" accept="image/gif,image/jpeg,image/jpg,image/png,image/svg" class="hidden" @change="handleFile" />
       <!-- 血条 -->
       <CharacterHpBar :hp="props.chara.embedCard.hp" :max-hp="props.chara.embedCard.maxHp" />
     </div>
@@ -31,9 +38,30 @@
 import { ISceneNpc, useSceneStore } from '../../../store/scene'
 import { DocumentTextIcon, MapPinIcon, TrashIcon, Square2StackIcon } from '@heroicons/vue/24/outline'
 import CharacterHpBar from './CharacterHpBar.vue'
+import { ref } from 'vue'
 
 const props = defineProps<{ chara: ISceneNpc }>()
 
 const sceneStore = useSceneStore()
 const showNpcCard = () => (sceneStore.currentCardNpc = props.chara)
+
+// 上传 npc 头像
+const realUploadBtn = ref<HTMLInputElement>()
+
+const handleFile = (e: Event) => {
+  const files = (e.target as HTMLInputElement).files
+  if (files && files.length > 0) {
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      const imageUrl = e.target!.result as string
+      // eslint-disable-next-line vue/no-mutating-props
+      props.chara.avatar = imageUrl
+    }
+    reader.readAsDataURL(files![0])
+  }
+}
+
+const uploadAvatar = () => {
+  realUploadBtn.value?.click()
+}
 </script>
