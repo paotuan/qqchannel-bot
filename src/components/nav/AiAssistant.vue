e935c7ca0d81311aab5c85bc8901bd08b403206e
maliut <lqn619@163.com>
2023-03-07 00:13:00 +0800

    feat: ai 助手

--- src/components/nav/AiAssistant.vue
+++ src/components/nav/AiAssistant.vue
@@ -6,30 +6,48 @@
     <div ref="panelEl" v-show="panelVisible" class="fixed z-50 w-[28rem] h-[84vh] bg-base-100 shadow-lg flex flex-col panel-card" :style="style">
       <h3 ref="panelTitleEl" class="cursor-move font-bold text-lg leading-10 pl-4 pt-2 flex-none">来点灵感</h3>
       <span class="absolute right-2 top-2">
-        <button class="btn btn-sm btn-circle btn-ghost" @click="chatStore.clearHistory()"><ArrowPathRoundedSquareIcon class="w-4 h-4" /></button>
-        <button class="btn btn-sm btn-circle btn-ghost" @click="panelVisible = false"><Cog6ToothIcon class="w-4 h-4" /></button>
-        <button class="btn btn-sm btn-circle btn-ghost" @click="panelVisible = false"><XMarkIcon class="w-4 h-4" /></button>
+        <span class="tooltip tooltip-left" data-tip="重置所有对话">
+          <button class="btn btn-sm btn-circle btn-ghost" @click="chatStore.clearHistory()"><ArrowPathRoundedSquareIcon class="w-4 h-4" /></button>
+        </span>
+        <span class="tooltip tooltip-left" data-tip="高级设置">
+          <button class="btn btn-sm btn-circle btn-ghost" @click="panelVisible = false"><Cog6ToothIcon class="w-4 h-4" /></button>
+        </span>
+        <span class="tooltip tooltip-left" data-tip="关闭窗口">
+          <button class="btn btn-sm btn-circle btn-ghost" @click="panelVisible = false"><XMarkIcon class="w-4 h-4" /></button>
+        </span>
       </span>
-      <div ref="conversationListEl" class="m-4 mt-0 p-2 flex-grow overflow-y-auto border-common">
-        <div>提示提示提示</div>
-        <div v-for="chat in chatStore.history" :key="chat.id" class="chat" :class="chat.role === 'user' ? 'chat-end' : 'chat-start'">
-          <div class="chat-bubble" :class="chat.role === 'user' ? 'chat-bubble-primary' : 'chat-bubble-accent'">{{ chat.content }}</div>
+      <div ref="conversationListEl" class="m-4 mt-0 p-2 flex-grow overflow-x-hidden overflow-y-auto border-common">
+        <div class="alert alert-info text-sm p-3">
+          <div>
+            <span>关于 AI 的使用帮助请<a class="link">查看这里</a>。服务器资源有限，请勿滥用。请考虑<a class="link">支持我们</a>，帮助维持这个功能的长期运行！</span>
+          </div>
         </div>
-        <div v-if="chatStore.chatError" class="chat chat-start">
-          <div class="chat-bubble chat-bubble-error">{{ chatStore.chatError }}</div>
+        <div v-for="chat in chatStore.history" :key="chat.id" class="chat relative group" :class="chat.role === 'user' ? 'chat-end' : 'chat-start'">
+          <div class="chat-bubble" :class="chat.isError ? 'chat-bubble-error' : chat.role === 'user' ? 'chat-bubble-primary' : 'chat-bubble-accent'">
+            {{ chat.content }}
+            <span v-if="chat.role !== 'user'" class="tooltip tooltip-left absolute bottom-0 -right-10 invisible group-hover:visible" data-tip="移除本轮对话">
+              <button class="btn btn-circle btn-error btn-sm " @click="chatStore.clearSingle(chat.id)">
+                <XMarkIcon class="w-4 h-4" />
+              </button>
+            </span>
+          </div>
         </div>
-        <Transition appear>
+        <Transition appear name="slide-fade">
           <div v-if="chatStore.chatLoading" class="chat chat-start">
             <div class="chat-bubble chat-bubble-accent">AI 助手思考中…</div>
           </div>
         </Transition>
       </div>
-      <div class="form-control flex-none w-full p-4 pt-0">
+      <div class="form-control flex-none w-full p-4 pt-0 dropdown dropdown-hover dropdown-top dropdown-end">
         <div class="input-group input-group-sm">
-          <input v-model="inputArea" type="text" placeholder="说些什么……" class="input input-bordered input-sm flex-grow" />
-<!--          <button class="btn btn-sm btn-secondary">预设</button>-->
-          <button class="btn btn-sm btn-primary" :class="{ loading: chatStore.chatLoading }" @click="send" @keyup.enter="send">发送</button>
+          <input v-model="inputArea" type="text" placeholder="说些什么……" class="input input-bordered input-sm flex-grow" @keyup.enter="send" />
+          <button tabindex="0" class="btn btn-sm btn-secondary">预设</button>
+          <button class="btn btn-sm btn-primary" :class="{ loading: chatStore.chatLoading }" @click="send">发送</button>
         </div>
+        <!-- input-group 和 dropdown 的组合，很怪，不过先这样吧 -->
+        <ul tabindex="0" class="dropdown-content menu menu-compact p-2 mx-8 shadow bg-base-100 rounded-box w-30">
+          <li v-for="item in presets" :key="item.key"><a @click="inputArea = item.content">{{ item.key }}</a></li>
+        </ul>
       </div>
     </div>
   </div>
@@ -38,7 +56,7 @@
 import { LightBulbIcon } from '@heroicons/vue/24/solid'
 import { ArrowPathRoundedSquareIcon, XMarkIcon, Cog6ToothIcon } from '@heroicons/vue/24/outline'
 import { ref, watch } from 'vue'
-import { useDraggable, useElementSize, useWindowSize } from '@vueuse/core'
+import { useDraggable, useWindowSize } from '@vueuse/core'
 import { useChannelStore } from '../../store/channel'
 import { useChatStore } from '../../store/chat'
 import { clamp } from 'lodash'
@@ -56,8 +74,8 @@ const { x: panelX, y: panelY, style } = useDraggable(panelEl, {
   handle: panelTitleEl,
   preventDefault: true,
   onMove: position => {
-    position.x = clamp(position.x, 0, width.value - 400) // todo
-    position.y = clamp(position.y, 0, height.value - 200)
+    position.x = clamp(position.x, 0, width.value - 420)
+    position.y = clamp(position.y, 0, height.value - 50)
   }
 })
 
@@ -68,21 +86,32 @@ const resetPosition = () => {
 
 // region 聊天列表自动滚动到最后
 const conversationListEl = ref<HTMLElement | null>(null)
-const { height: conversationListHeight } = useElementSize(conversationListEl)
-watch(conversationListHeight, (value, oldValue) => {
-  if (value > oldValue && conversationListEl.value) {
-    conversationListEl.value.scrollTop = conversationListEl.value.scrollHeight
+watch(() => chatStore.history.length, (value, oldValue) => {
+  if (value > oldValue) {
+    setTimeout(() => {
+      if (conversationListEl.value) {
+        conversationListEl.value.scrollTop = conversationListEl.value.scrollHeight
+      }
+    }, 100)
   }
 })
 
 // region 发送逻辑
 const inputArea = ref('')
 const send = () => {
+  if (chatStore.chatLoading) return
   if (!inputArea.value.trim()) return
   chatStore.request(inputArea.value)
   inputArea.value = ''
 }
 
+// 预设
+const presets = [
+  { key: '场景生成', content: '请描述一个傍晚时分阴森诡异的树林场景，200字左右' },
+  { key: '人物导入', content: '请描述一个名叫阿瑟的神父和一个名叫大卫的战地医生，他们认识的背景故事，300字左右' },
+  { key: '总结对话', content: '请总结上述对话的内容，实现减少token的同时，保证对话的质量' },
+  { key: '继续', content: 'continue' }
+]
 </script>
 <style scoped>
 .panel-card {
--- src/store/chat.ts
+++ src/store/chat.ts
@@ -16,11 +16,14 @@ export const useChatStore = defineStore('chat', () => {
   const systemPrompt = ref('')
   const history = reactive<IMessage[]>([])
   const chatLoading = ref(false)
-  const chatError = ref('') // todo 干掉
   const apiKey = ref('')
 
   const _getBodyAndRecord = (content: string) => {
-    const body = history.map(item => ({ role: item.role, content: item.content })) // todo 过滤掉当次失败的请求
+    // 如果有失败的消息，则过滤掉
+    if (history.at(-1)?.isError) {
+      clearSingle(history.at(-1)!.id)
+    }
+    const body = history.map(item => ({ role: item.role, content: item.content }))
     const userMessage = { role: 'user' as const, content: content.trim() }
     body.push(userMessage)
     if (systemPrompt.value.trim()) {
@@ -31,65 +34,68 @@ export const useChatStore = defineStore('chat', () => {
     return body
   }
 
-  const _requestInner = async (body: IMessageForRequest[]) => {
+  const _requestInner = (body: IMessageForRequest[]) => {
     // todo report
     const bot = useBotStore()
-    const response = await fetch('https://chatapi.paotuan.io/channel-bot', {
+    return fetch('https://chatapi.paotuan.io/channel-bot', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ messages: body, auth: auth(`${bot.appid}.${bot.token}`) })
     })
-    if (response.status !== 200) {
-      const body = await response.text()
-      chatError.value = `Failed to send message. HTTP ${response.status} - ${body}`
-    } else {
-      const res = await response.json()
-      const content = res.choices[0].message.content.trim()
-      history.push({ role: 'assistant', content, id: nanoid() })
-    }
   }
 
-  const _requestOfficial = async (body: IMessageForRequest[]) => {
-    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+  const _requestOfficial = (body: IMessageForRequest[]) => {
+    return fetch('https://api.openai.com/v1/chat/completions', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${apiKey.value.trim()}` },
       body: JSON.stringify({ model: 'gpt-3.5-turbo', messages: body })
     })
-    if (response.status !== 200) {
-      const body = await response.text()
-      chatError.value = `Failed to send message. HTTP ${response.status} - ${body}`
-    } else {
-      const res = await response.json()
-      const content = res.choices[0].message.content.trim()
-      history.push({ role: 'assistant', content, id: nanoid() })
-    }
   }
 
   const request = async (content: string) => {
+    if (chatLoading.value) return // loading 中不允许请求
     const body = _getBodyAndRecord(content)
     try {
       chatLoading.value = true
-      chatError.value = ''
-      await (apiKey.value.trim() ? _requestOfficial : _requestInner)(body)
+      const response = await (apiKey.value.trim() ? _requestOfficial : _requestInner)(body)
+      if (response.status !== 200) {
+        const body = await response.text()
+        history.push({ role: 'assistant', content: `Failed to send message. HTTP ${response.status} - ${body}`, id: nanoid(), isError: true })
+      } else {
+        const res = await response.json()
+        const content = res.choices[0].message.content.trim()
+        history.push({ role: 'assistant', content, id: nanoid() })
+      }
+      // await new Promise(resolve => {
+      //   setTimeout(() => {
+      //     history.push({ role: 'assistant', content: '我是一名人工智能语言模型，被设计用于提供基于自然语言的智能交互，以帮助人类更高效地处理信息和解决问题。我能够理解和解释人类的语言，并给出与之相关的答案和建议。我没有具体的身份，也没有情感和创造力，但我会尽力帮助提问者解决问题，并通过不断的学习和进化来提高自己的智能和能力。', id: nanoid() })
+      //     resolve('')
+      //   }, 2000)
+      // })
     } catch (e) {
       console.log(e)
-      chatError.value = '发生了错误'
+      history.push({ role: 'assistant', content: '发生了错误', id: nanoid(), isError: true })
     } finally {
       chatLoading.value = false
     }
   }
 
-  const clearHistory = () => history.length = 0
+  const clearHistory = () => {
+    if (chatLoading.value) return // loading 中先不允许操作吧，以防止潜在的 bug
+    history.length = 0
+  }
 
   // 清除单轮对话
   const clearSingle = (id: string) => {
     const index2remove = history.findIndex(item => item.id === id)
-    if (index2remove >= 0) {
+    if (index2remove > 0) {
       history.splice(index2remove - 1, 2) // 发送方一起删
+    } else if (index2remove === 0) {
+      history.splice(0, 1)
     }
   }
 
-  return { systemPrompt, history, chatLoading, chatError, request, clearHistory, clearSingle }
+  return { systemPrompt, history, chatLoading, request, clearHistory, clearSingle }
 })
 
 function auth(data: string) {
