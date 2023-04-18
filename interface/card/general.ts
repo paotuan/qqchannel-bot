b0b86be0419deb0db8342b20e66f9934d3dcb482
maliut <lqn619@163.com>
2023-04-18 18:42:22 +0800

    feat: 人物卡修改部分字段名&内部 key 统一大写

--- interface/card/general.ts
+++ interface/card/general.ts
@@ -1,4 +1,4 @@
-import { ICard } from './index'
+import type { ICard } from './index'
 
 export interface IGeneralCardData {
   type: 'general'
@@ -11,7 +11,7 @@ export interface IGeneralCardData {
 }
 
 export class GeneralCard implements ICard {
-  private data: IGeneralCardData
+  private readonly data: IGeneralCardData
 
   get type() {
     return this.data.type
@@ -21,27 +21,73 @@ export class GeneralCard implements ICard {
     return this.data.version
   }
 
+  get id() {
+    return this.data.id
+  }
+
+  get name() {
+    return this.data.name
+  }
+
+  get hp() {
+    return this.getEntry('HP')?.value
+  }
+
+  get maxHp() {
+    return this.getEntry('MAXHP')?.value
+  }
+
   constructor(data: IGeneralCardData) {
     this.data = data
   }
 
-  getAbility(expression: string) {
-    return undefined
+  getAbility(input: string) {
+    const key = input.toUpperCase()
+    const value = this.data.abilities[key]
+    if (typeof value !== 'undefined') {
+      return { input, key, value }
+    } else {
+      return undefined
+    }
   }
 
   setAbility(name: string, value: string) {
+    this.data.abilities[name.toUpperCase()] = value
+    return true
   }
 
   removeAbility(name: string) {
+    const key = name.toUpperCase()
+    if (typeof this.data.abilities[key] !== 'undefined') {
+      delete this.data.abilities[key]
+      return true
+    } else {
+      return false
+    }
   }
 
-  getEntry(expression: string) {
-    return undefined
+  getEntry(input: string) {
+    const key = input.toUpperCase()
+    const value = this.data.skills[key]
+    if (typeof value !== 'undefined') {
+      return { input, key, value, isTemp: false }
+    } else {
+      return undefined
+    }
   }
 
   setEntry(name: string, value: number) {
+    this.data.skills[name.toUpperCase()] = value
+    return true
   }
 
   removeEntry(name: string) {
+    const key = name.toUpperCase()
+    if (typeof this.data.skills[key] !== 'undefined') {
+      delete this.data.skills[key]
+      return true
+    } else {
+      return false
+    }
   }
 }
