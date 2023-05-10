59b071fbd68cfecfc1563373517019d6913329e3
maliut <lqn619@163.com>
2023-05-10 19:18:25 +0800

    feat: card type badge

--- src/views/CardPanel/CardPanel.vue
+++ src/views/CardPanel/CardPanel.vue
@@ -17,12 +17,12 @@
       <div class="flex flex-col gap-2">
         <h3 class="font-bold">人物卡列表：</h3>
         <div v-for="card in cardStore.displayCardList" :key="card.name" class="flex gap-2">
-          <button class="btn w-40 gap-2 justify-start flex-nowrap"
+          <button class="btn w-40 gap-2 justify-start flex-nowrap relative"
                   :class="selectedCard === card ? 'btn-secondary' : 'btn-ghost border border-base-300'"
                   :title="card.name"
                   @click="cardStore.selectCard(card.name)">
-            <DocumentTextIcon class="w-6 h-6 flex-none"/>
-            <span class="truncate">{{ card.name }}{{ cardStore.isEdited(card.name) ? ' *' : '' }}</span>
+            <CardTypeBadge :type="card.type" class="absolute -top-1.5 -left-1.5" />
+            <span class="truncate translate-y-1/4">{{ card.name }}{{ cardStore.isEdited(card.name) ? ' *' : '' }}</span>
             <CheckCircleIcon v-show="selectedCard === card" class="w-6 h-6 ml-auto flex-none" />
           </button>
           <user-selector :user-id="cardStore.linkedUserOf(card.name) || null" @select="cardStore.requestLinkUser(card.name, $event?.id)" />
@@ -38,12 +38,13 @@
 </template>
 <script setup lang="ts">
 import CardImportDialog from './CardImportDialog.vue'
-import { EyeSlashIcon, EyeIcon, DocumentTextIcon, CheckCircleIcon } from '@heroicons/vue/24/outline'
+import { EyeSlashIcon, EyeIcon, CheckCircleIcon } from '@heroicons/vue/24/outline'
 import { useCardStore } from '../../store/card'
 import UserSelector from './UserSelector.vue'
 import { computed, provide } from 'vue'
 import { SELECTED_CARD } from './utils'
 import CocCardDisplay from './display/CocCardDisplay.vue'
+import CardTypeBadge from './CardTypeBadge.vue'
 
 const cardStore = useCardStore()
 const selectedCard = computed(() => cardStore.selectedCard)
