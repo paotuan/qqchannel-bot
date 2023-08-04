64de577ffd8cd808353cfc90182d6b2b0de47d84
maliut <lqn619@163.com>
2023-08-04 18:15:42 +0800

    feat: 使用右键插入 ^变量名

--- src/views/ConfigPanel/customText/ArgButton.vue
+++ src/views/ConfigPanel/customText/ArgButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="btn btn-xs" :class="data.section ? 'btn-warning' : 'btn-success'" @click="onClick">{{ data.name }}</button>
+  <button class="btn btn-xs" :class="data.section ? 'btn-warning' : 'btn-success'" @click="onClick" @contextmenu.prevent="onClick(true)">{{ data.name }}</button>
 </template>
 <script setup lang="ts">
 import type { ICustomTextMetaItem } from './customTextMeta'
@@ -9,9 +9,9 @@ const props = defineProps<{ data: ICustomTextMetaItem['args'][number] }>()
 const emit = defineEmits<{ (e: 'click', value: { segment: string, insertAt: number }): void }>()
 const { data } = toRefs(props)
 
-const onClick = () => {
+const onClick = (contextMenu = false) => {
   if (data.value.section) {
-    const segment = `{{#${data.value.name}}}`
+    const segment = contextMenu ? `{{^${data.value.name}}}` : `{{#${data.value.name}}}`
     const closeSegment = `{{/${data.value.name}}}`
     emit('click', { segment: segment + closeSegment, insertAt: segment.length })
   } else {
