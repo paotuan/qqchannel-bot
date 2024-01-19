26ff7f2a44e7cb32170c6bedee4efedd95593e59
maliut <lqn619@163.com>
2024-01-19 18:20:46 +0800

    feat: 插件开关与各项目的联动

--- src/views/ConfigPanel/plugin/PluginEditor.vue
+++ src/views/ConfigPanel/plugin/PluginEditor.vue
@@ -1,8 +1,16 @@
 <template>
   <div v-if="plugin" class="collapse" :class="{ 'collapse-open': isOpen, 'collapse-close': !isOpen }">
     <div class="collapse-title text-md font-medium flex items-center gap-2 cursor-pointer" @click="isOpen = !isOpen">
-      <input v-model="item.enabled" type="checkbox" class="toggle toggle-sm" @click.stop />
+      <input v-model="item.enabled" type="checkbox" class="toggle toggle-sm" @click.stop @change="toggleEnabled($event.target.checked)" />
       <span class="inline-flex items-center gap-1 group">{{ plugin.name }}</span>
+      <span>
+        <span v-if="plugin.customReply.length > 0" class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">自定义回复</span>
+        <span v-if="plugin.customText.length > 0" class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">自定义文案</span>
+<!--        <span v-if="plugin.rollDecider.length > 0" class="bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">检定规则</span>-->
+        <span v-if="plugin.aliasRoll.length > 0" class="bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">别名指令</span>
+<!--        <span class="bg-purple-100 text-purple-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">钩子函数</span>-->
+<!--        <span class="bg-pink-100 text-pink-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">Pink</span>-->
+      </span>
       <span class="flex-grow text-right">
         <button class="btn btn-circle btn-ghost btn-sm" @click.stop="reloadPlugin(plugin.id)">
           <ArrowPathIcon class="h-4 w-4" />
@@ -18,6 +26,7 @@
 import { computed, ref, toRefs } from 'vue'
 import { usePluginStore } from '../../../store/plugin'
 import { ArrowPathIcon } from '@heroicons/vue/24/outline'
+import { useConfigStore } from '../../../store/config'
 
 interface Props { item: { id: string, enabled: boolean }, defaultOpen: boolean }
 
@@ -26,9 +35,28 @@ const { item } = toRefs(props)
 
 const pluginStore = usePluginStore()
 const plugin = computed(() => pluginStore.getPlugin(item.value.id))
+const configStore = useConfigStore()
+const config = computed(() => configStore.config!)
 
 // 面板展开状态
 const isOpen = ref(props.defaultOpen)
+
+// 切换插件的开关状态
+const toggleEnabled = (enabled: boolean) => {
+  const manifest = plugin.value!
+  if (enabled) {
+    manifest.customReply.forEach(elem => config.value.customReplyIds.push({ id: `${manifest.id}.${elem.id}`, enabled: elem.defaultEnabled }))
+    manifest.aliasRoll.forEach(elem => config.value.aliasRollIds.push({ id: `${manifest.id}.${elem.id}`, enabled: elem.defaultEnabled }))
+    // manifest.rollDecider.forEach(elem => config.value.rollDeciderIds.push({ id: `${manifest.id}.${elem.id}`, enabled: elem.defaultEnabled }))
+    manifest.customText.forEach(elem => config.value.customTextIds.push({ id: `${manifest.id}.${elem.id}`, enabled: elem.defaultEnabled }))
+  } else {
+    // 关闭这个插件的所有 element
+    manifest.customReply.forEach(elem => configStore.deleteCustomReplyConfig(`${manifest.id}.${elem.id}`))
+    manifest.aliasRoll.forEach(elem => configStore.deleteAliasRollConfig(`${manifest.id}.${elem.id}`))
+    // manifest.rollDecider.forEach(elem => configStore.deleteRollDeciderConfig(`${manifest.id}.${elem.id}`))
+    manifest.customText.forEach(elem => configStore.deleteCustomTextConfig(`${manifest.id}.${elem.id}`))
+  }
+}
 
 // reload
 const reloadPlugin = (id: string) => {
--- src/views/ConfigPanel/plugin/PluginPanel.vue
+++ src/views/ConfigPanel/plugin/PluginPanel.vue
@@ -4,6 +4,12 @@
       <h2>插件管理：</h2>
       <HelpButton path="/config/alias/" />
     </div>
+    <div class="mb-4 flex justify-between">
+      <div class="tooltip" data-tip="敬请期待">
+        <button class="btn btn-primary gap-2" disabled><PlusCircleIcon class="w-6 h-6"/>插件市场</button>
+      </div>
+      <button class="btn btn-ghost gap-2"><ArrowPathIcon class="h-4 w-4" />重载所有插件</button>
+    </div>
     <div class="card card-compact w-full bg-base-100 shadow-lg">
       <PluginList />
     </div>
@@ -12,6 +18,7 @@
 <script setup lang="ts">
 import HelpButton from '../HelpButton.vue'
 import PluginList from './PluginList.vue'
+import { ArrowPathIcon, PlusCircleIcon } from '@heroicons/vue/24/outline'
 </script>
 <style scoped>
 h2 {
