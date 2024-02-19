6885a0888d3d76f7167927f20aee2b55ae8ead75
maliut <lqn619@163.com>
2024-02-19 21:03:26 +0800

    feat: 钩子函数添加说明

--- src/views/ConfigPanel/hook/HookPanel.vue
+++ src/views/ConfigPanel/hook/HookPanel.vue
@@ -1,5 +1,7 @@
 <template>
   <section>
+    <h2>钩子函数：</h2>
+    <div class="text-sm mb-2">钩子函数主要提供给插件使用，用户通常无需修改此处设置，或根据插件的说明进行修改。</div>
     <template v-for="module in moduleList" :key="module.value">
       <HelpCollapseArea :title="module.label" :main="true" :default-expand="true">
         <div class="card card-compact w-full bg-base-100 shadow-lg">
@@ -20,3 +22,8 @@ const moduleList: { label: string, value: HookModule }[] = [
   { label: '人物卡数值变化', value: 'onCardEntryChange' }
 ]
 </script>
+<style scoped>
+h2 {
+  @apply font-bold leading-10;
+}
+</style>
