bab91bf1ae22b7135204fbab4b41176f638e4be3
maliut <lqn619@163.com>
2024-06-25 17:09:25 +0800

    feat: wss dispatch

--- apps/server/src/app/http.ts
+++ apps/server/src/app/http.ts
@@ -10,6 +10,20 @@ export function setupServer(port: number) {
   const syncServer = createWss({ persistenceDir })
   const bizServer = new Wss().server
 
+  // https://github.com/websockets/ws?tab=readme-ov-file#multiple-servers-sharing-a-single-https-server
+  httpServer.on('upgrade', (request, socket, head) => {
+    const { pathname } = new URL(request.url || '', 'wss://base.url')
+    if (pathname === '/') {
+      bizServer.handleUpgrade(request, socket, head, ws => {
+        bizServer.emit('connection', ws, request)
+      })
+    } else {
+      syncServer.handleUpgrade(request, socket, head, ws => {
+        syncServer.emit('connection', ws, request)
+      })
+    }
+  })
+
   httpServer.listen(port)
   console.log('服务已启动，端口号 ' + port)
 }
