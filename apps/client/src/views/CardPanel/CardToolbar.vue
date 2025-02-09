7870233a82192c98aed7167ec419db7d270f5004
maliut <lqn619@163.com>
2025-02-09 21:31:44 +0800

    feat: turn exist card into template

--- apps/client/src/views/CardPanel/CardToolbar.vue
+++ apps/client/src/views/CardPanel/CardToolbar.vue
@@ -1,5 +1,6 @@
 <template>
   <CardAddAttribute :dnd="selectedCard.type === 'dnd'" @submit="addSkillsBatch" />
+  <button class="btn btn-xs" @click="createTemplate">创建模板</button>
   <button class="btn btn-xs btn-error" @click="deleteCard">删除人物卡</button>
 </template>
 <script setup lang="ts">
@@ -7,6 +8,7 @@ import CardAddAttribute from './CardAddAttribute.vue'
 import { useCurrentSelectedCard } from './utils'
 import { useCardStore } from '../../store/card'
 import { addAttributesBatch } from '../../store/card/importer/utils'
+import { cloneDeep } from 'lodash'
 
 const cardStore = useCardStore()
 const selectedCard = useCurrentSelectedCard()! // 外面确保 selectedCard 一定存在
@@ -21,5 +23,21 @@ const deleteCard = () => {
   if (window.confirm('确定要删除这张人物卡吗？')) {
     cardStore.deleteCard(selectedCard.value.name)
   }
+}
+
+// 将人物卡转为模板
+const createTemplate = () => {
+  // 找到合法的名称
+  const originName = selectedCard.value.name + '模板'
+  let name = originName
+  let count = 1
+  while (cardStore.of(name)) {
+    name = `${originName}(${++count})`
+  }
+  const newCardData = cloneDeep(selectedCard.value.data)
+  newCardData.isTemplate = true
+  newCardData.created = newCardData.lastModified = Date.now()
+  newCardData.name = name
+  cardStore.importCard(newCardData)
 }
 </script>
