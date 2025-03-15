ded9f4e122cdbd7f34ded63768f2cdc6d0f90538
maliut <lqn619@163.com>
2025-03-16 00:59:45 +0800

    update version

--- packages/card/src/utils/version.ts
+++ packages/card/src/utils/version.ts
@@ -1,1 +1,1 @@
-export const VERSION_CODE = 46
+export const VERSION_CODE = 47
--- packages/types/src/version.ts
+++ packages/types/src/version.ts
@@ -1,2 +1,2 @@
-export const VERSION_NAME = 'v2.1.0'
-export const VERSION_CODE = 46
+export const VERSION_NAME = 'v2.1.1'
+export const VERSION_CODE = 47
