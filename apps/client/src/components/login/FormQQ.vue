2b4824ae379eb9402e575aed0c89b63524ba45af
maliut <lqn619@163.com>
2025-05-13 18:02:22 +0800

    feat: qq login add new options

--- apps/client/src/components/login/FormQQ.vue
+++ apps/client/src/components/login/FormQQ.vue
@@ -46,14 +46,54 @@
         </label>
       </div>
     </div>
+    <button class="btn btn-link px-1" @click="advancedSettingsVisible = true">更多设置</button>
+    <d-modal v-model:visible="advancedSettingsVisible" title="更多设置">
+      <div class="form-control">
+        <label class="label">
+          <span class="label-text font-bold">QQ API 地址</span>
+        </label>
+        <input v-model="model.endpoint" type="text" placeholder="https://api.sgroup.qq.com/" class="input input-bordered w-full" />
+      </div>
+      <div class="form-control">
+        <label class="label">
+          <span class="label-text font-bold">连接方式</span>
+        </label>
+        <d-native-select v-model="model.protocol" :options="protocolOptions" select-class="select-bordered" />
+      </div>
+      <template v-if="model.protocol === 'webhook'">
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text font-bold">监听路径</span>
+          </label>
+          <label class="input input-bordered flex items-center gap-2">
+            /<input v-model="model.path" type="text" class="grow" />
+          </label>
+        </div>
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text font-bold">端口号</span>
+          </label>
+          <d-number-input v-model="model.port" class="input input-bordered w-full" />
+        </div>
+      </template>
+    </d-modal>
   </div>
 </template>
 <script setup lang="ts">
 import { useBotStore } from '../../store/bot'
-import { computed } from 'vue'
-import { IBotConfig_QQ } from '@paotuan/types'
+import { computed, ref } from 'vue'
+import type { IBotConfig_QQ } from '@paotuan/types'
+import DModal from '../../dui/modal/DModal.vue'
+import DNativeSelect from '../../dui/select/DNativeSelect.vue'
+import DNumberInput from '../../dui/input/DNumberInput.vue'
 
 const bot = useBotStore()
-const model = computed(() => bot.formModel as IBotConfig_QQ)
+const model = computed(() => bot.formModel as Required<IBotConfig_QQ>)
+const advancedSettingsVisible = ref(false)
+
+const protocolOptions = [
+  { label: 'Websocket', value: 'websocket' },
+  { label: 'Webhook', value: 'webhook' },
+]
 </script>
 
